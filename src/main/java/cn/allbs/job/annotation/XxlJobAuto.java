c8941f2ca2c0afb0ab31bf5e1f9b1cdccc8263e4
chenqi <chenqi92104@icloud.com>
2023-06-19 16:53:37 +0800

    🔧 update: 添加执行器路由策略说明

--- src/main/java/cn/allbs/job/annotation/XxlJobAuto.java
+++ src/main/java/cn/allbs/job/annotation/XxlJobAuto.java
@@ -59,7 +59,17 @@ public @interface XxlJobAuto {
     String misfireStrategy() default "DO_NOTHING";
 
     /**
-     * 执行器路由策略 ()
+     * 执行器路由策略
+     * FIRST 第一个
+     * LAST 最后一个
+     * ROUND 轮询
+     * RANDOM 随机
+     * CONSISTENT_HASH 一致性HASH
+     * LEAST_FREQUENTLY_USED 最不经常使用
+     * LEAST_RECENTLY_USED 最近最久未使用
+     * FAILOVER 故障转移
+     * BUSYOVER 忙碌转移
+     * SHARDING_BROADCAST 分片广播
      */
     String executorRouteStrategy() default "FIRST";
 
